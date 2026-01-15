ae412ba122ede2795ae94b8bd3a28d1fb22972da
SheilaCG-dev <sheila.crespo.gomez@accenture.com>
2026-01-15 11:49:36 +0000

    commit

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,10 +13,40 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'teams', views.TeamViewSet)
+router.register(r'activities', views.ActivityViewSet)
+router.register(r'workouts', views.WorkoutViewSet)
+router.register(r'leaderboard', views.LeaderboardViewSet)
+
+def get_api_url(component):
+    codespace = os.environ.get('CODESPACE_NAME')
+    if codespace:
+        return f'https://{codespace}-8000.app.github.dev/api/{component}/'
+    return f'http://localhost:8000/api/{component}/'
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': get_api_url('users'),
+        'teams': get_api_url('teams'),
+        'activities': get_api_url('activities'),
+        'workouts': get_api_url('workouts'),
+        'leaderboard': get_api_url('leaderboard'),
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('octofit_tracker.urls')),
+    path('api/', api_root, name='api-root'),
+    path('api/', include(router.urls)),
 ]
